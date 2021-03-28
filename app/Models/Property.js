f23fbcfc0701ad01841f8da679ee378ce171b5ce
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-28 11:27:50 -0300

    Add references image and user on property

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -4,7 +4,13 @@
 const Model = use('Model')
 
 class Property extends Model {
-  
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  images () {
+    return this.hasMany('App/Models/Image')
+  }
 }
 
 module.exports = Property
