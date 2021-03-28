c9d928b931fbcb8794c575dcdf99ca89d4cbc128
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-28 16:58:11 -0300

    Fix parameter auth in Store action

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -34,7 +34,7 @@ class PropertyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ auth, request, response }) {
     const { id } = auth.user
     const data = request.only([
       'title',
