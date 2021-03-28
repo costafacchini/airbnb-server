1c6d2fb970c2b3f7dda7e9df1df19fff812fcf70
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-28 11:19:10 -0300

    Create routes and controllers to create and autenticate users

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,9 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
