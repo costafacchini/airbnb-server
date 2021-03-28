da9a05301d47f4c74c3570ed50c0379014f076eb
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-28 11:29:55 -0300

    Create property routes

--- start/routes.js
+++ start/routes.js
@@ -18,6 +18,9 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
 
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
